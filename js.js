e7c826cf32938ed27849e3d9ef5e17e42eb83ee4
Mehulsingh1010 <137038233+Mehulsingh1010@users.noreply.github.com>
2023-12-20 19:55:02 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+
+
+
+var tl =gsap.timeline()
+
+tl.to("#page1",{
+    y:"100vh",
+    scale:0.5,
+    duration:0
+})
+tl.to("#page1",{
+    y:"0vh",
+    duration:0.8,
+    delay:1
+
+})
+
+tl.to("#page1",{
+    rotate:360,
+    scale:1
+})
